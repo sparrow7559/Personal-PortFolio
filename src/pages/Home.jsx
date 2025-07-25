c0c6d8cf476535a0adf9cd32b1d6268cfdf81670
sparrow7559 <virajgupta2604@gmail.com>
2025-07-26 00:03:48 +0530

    Add Navbar component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ThemeToggle } from "../components/ThemeToggle";
 import StarBackground from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle />
       <StarBackground />
+      <Navbar />
     </div>
   );
 };
