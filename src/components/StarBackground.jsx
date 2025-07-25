48e6e0f1876a1aef87d06b5971350f35edb4ec94
sparrow7559 <virajgupta2604@gmail.com>
2025-07-25 18:52:52 +0530

    handle star resize clutter

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -7,6 +7,14 @@ const StarBackground = () => {
   useEffect(() => {
     generateStars();
     generateMeteors();
+
+    const handleResize = () => {
+      generateStars();
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   const generateMeteors = () => {
