aee21153a1275d52e362e80cddaac2324dd4dad1
sparrow7559 <virajgupta2604@gmail.com>
2025-07-25 18:34:08 +0530

    Setup and star background

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ThemeToggle } from "../components/ThemeToggle";
+import StarBackground from "../components/StarBackground";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+      <StarBackground />
+    </div>
+  );
+};
+
+export default Home;
